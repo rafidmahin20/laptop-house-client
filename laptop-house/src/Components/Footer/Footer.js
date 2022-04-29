50af014fce2489af3a8fa5a1edc69a9daf3c702a
rafidmahin20 <submanmahin@gmail.com>
2022-04-29 17:25:48 +0600

    edited

---
+++ laptop-house/src/Components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import facebook from '../../Images/social/facebook.png';
+import instagram from '../../Images/social/instagram.png';
+import twitter from '../../Images/social/twitter.png'
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className=''>
+            <div className='h-full grid grid-cols-1 md:grid-cols-2 footer-bg'>
+                <div className='text-white py-5 px-5'>
+                    <h3 className='footer-heading'>Laptop House</h3>
+                    <br/>
+                    About us
+                    <br/>
+                    FAQs
+                    <br/>
+                    Terms & Conditions
+                    <br/>
+                    Delivery & Collections
+                    <br/>
+                    Privacy Policy
+                </div>
+                <div className='text-white-400 py-5 px-5 mt-2 mx-auto'>
+                    <h3 className='footer-info'>Follow us on</h3>
+                    <div>
+                        <img src={facebook} alt=""></img>
+                        <img className='' src={instagram} alt=""></img>
+                        <img className='' src={twitter} alt=""></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </footer>
+    );
+};
+
+export default Footer;
