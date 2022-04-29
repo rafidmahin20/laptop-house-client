eb628d9a53a9788cc2ddb7be103180cc8ffd9638
rafidmahin20 <submanmahin@gmail.com>
2022-04-29 12:37:23 +0600

    edited

--- laptop-house/src/Components/Firebase/Header/Header.js
+++ laptop-house/src/Components/Firebase/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div>
+        <div className='sticky top-0 bg-slate-400 px-12 py-8 flex justify-center md:justify-between'>
+            <div className='hidden md:block'>
+                <Link to='/' className='font-sans font-bold text-4xl text-emerald-700'>Illusion Wedding</Link>
+            </div>
+            <div className='flex gap-3 text-xl'>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/inventorypage'>Inventory</CustomLink>
+                <CustomLink to='/blogs'>Blogs</CustomLink>
+            </div>
             
         </div>
     );
