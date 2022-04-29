c7e17852656d92affabacce24960272fa0543fd0
rafidmahin20 <submanmahin@gmail.com>
2022-04-30 01:09:10 +0600

    edited

--- laptop-house/src/Components/Home/Home.js
+++ laptop-house/src/Components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import pic1 from '../../Images/pic1.jpg'
+import useInventory from '../Hooks/useInventory';
+import Inventory from '../Inventory/Inventory';
 
 const Home = () => {
+    const [products] = useInventory();
     return (
-        <section className='px-4 pt-10 pb-10 mx-auto max-w-7xl md:px-24'>
+        <main>
+            <section className='px-4 pt-10 pb-10 mx-auto max-w-7xl md:px-24'>
                 <div className='h-full grid grid-cols-1 gap-20 md:grid-cols-2'>
                     <div className='order-2 md:order-1'>
                         <h1 className='text-red-500 font-bold text-4xl'>Creates Beauty</h1>
@@ -16,6 +20,17 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+             <section>
+             <h1 className='text-center text-4xl font-sans font-bold text-cyan-700 pt-10 pb-10'>Products</h1>
+             <div className='grid gap-4 grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 pt-10 pb-10'>
+                 {
+                     products.slice(0,6).map(
+                         (product, index) => <Inventory key={index} product={product}/>
+                     )
+                 }
+             </div>
+             </section>
+        </main>
     );
 };
 
