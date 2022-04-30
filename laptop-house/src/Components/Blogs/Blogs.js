4b15cbebce9ae6565a200a0e7868700b2b945ff6
rafidmahin20 <submanmahin@gmail.com>
2022-04-30 14:34:21 +0600

    edited

--- laptop-house/src/Components/Blogs/Blogs.js
+++ laptop-house/src/Components/Blogs/Blogs.js
@@ -6,10 +6,32 @@ const Blogs = () => {
       <div className="grid grid-cols-1 gap-24 md:grid-cols-2">
         <div>
           <h3 className="mt-10 mb-3 font-bold text-gray-900">
-          Difference between javascript and nodejs
+            Difference between javascript and nodejs
           </h3>
-          <p className='text-gray-600'>
-            -  js is client side coding. Node js enables server side coding writtrn in javascript. Java script is language. Node js is not just a language but it has v8 engine that runs javascript on the server side. Javascript runs inside the browser. Nodejs runs outside the browser.
+          <p className="text-gray-600">
+            - js is client side coding. Node js enables server side coding
+            writtrn in javascript. Java script is language. Node js is not just
+            a language but it has v8 engine that runs javascript on the server
+            side. Javascript runs inside the browser. Nodejs runs outside the
+            browser.
+          </p>
+          <h3 className="mt-10 mb-3 font-bold text-gray-900">
+           What is the purpose of jwt and how does it work
+          </h3>
+          <p className="text-gray-600">
+            - JWT also known as json web token provides you a way of represnting claims to be transfered between two parties. it is Manily used for authorization. JWT consists of three parts. Frist is header second is payload and third is signature. The header and payload are base64 encoded strings it can be decoded without any need for password so anyone can read the plain text format.
+          </p>
+        </div>
+        <div>
+          <h3 className="mt-10 mb-3 font-bold text-gray-900">
+            Differences between sql and nosql databases.
+          </h3>
+          <p className="text-gray-600">
+            - SQL is generally used in relational database management system.
+            NoSQL is used for non-relational distributed database system. In SQL
+            structured data can be stored in tables. Using json data
+            unstructured data can be stored. In SQL the schemas are static. In
+            NoSQL the schemas are dynamic.
           </p>
         </div>
       </div>
