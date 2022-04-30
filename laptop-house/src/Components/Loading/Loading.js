e60106174814a2982dda48bd9a560f5cae1dd1f0
rafidmahin20 <submanmahin@gmail.com>
2022-04-30 22:29:30 +0600

    edited

---
+++ laptop-house/src/Components/Loading/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <div
+          className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+          role="status"
+        >
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- laptop-house/src/Components/SignUp/SignUp.js
+++ laptop-house/src/Components/SignUp/SignUp.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import google from '../../Images/social/google.png';
+import auth from '../Firebase/Firebase.init'
+import Loading from '../Loading/Loading';
 
 const SignUp = () => {
     const navigate = useNavigate();
     const navigateToLogin = () => {
         navigate('/login');
     }
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+      if(user){
+        navigate('/login');
+      }
+      if(loading){
+          return <Loading/>
+      }
+
+      const handleSignUp = async event =>{
+          event.preventDefault();
+          const name = event.target.name.value;
+          const email = event.target.email.value;
+          const password = event.target.password.value;
+          await createUserWithEmailAndPassword(email, password);
+          
+      }
     return (
         <div className='flex items-center justify-center mt-20'>
         <div className='shadow-lg m-10 p-10 sm:w-96 md:w-96 lg:96 2xl:96 rounded border border-zinc-400'>
             <h2 className='text-3xl text-center font-bold mb-6'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className='mx-auto w-80  mb-5'>
-                <input type="text"  className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Fullname"/>
+                <input type="text" name='name' className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Fullname"/>
                 </div>
                 <div className='mx-auto w-80  mb-5'>
-                <input type="email"  className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="email" required/>
+                <input type="email" name='email' className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="email" required/>
                 </div>
                 <div className='mx-auto w-80  mb-5'>
-                <input type="password" id="floating_filled" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="password" required/>
+                <input type="password" name='password' id="floating_filled" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="password" required/>
                 </div>
                 <div className='flex justify-center'>
-                <input type='submit' className="text-white bg-blue-700 font-medium rounded-lg text-sm px-32 py-2.5 text-center mb-3" value="Login" />
+                <input type='submit' className="text-white bg-blue-700 font-medium rounded-lg text-sm px-32 py-2.5 text-center mb-3" value="Signup" />
                 </div>
             </form>
             <div className='flex items-center '>
