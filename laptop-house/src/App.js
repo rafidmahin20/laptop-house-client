e7526153101e491ce7597c4e3c1dcd24c118ec87
rafidmahin20 <submanmahin@gmail.com>
2022-05-01 00:23:48 +0600

    edited

--- laptop-house/src/App.js
+++ laptop-house/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AboutUs from './Components/About Us/AboutUs';
+import AddInventory from './Components/AddInventory/AddInventory';
 import Blogs from './Components/Blogs/Blogs';
 import Header from './Components/Firebase/Header/Header';
 import Footer from './Components/Footer/Footer';
@@ -8,6 +9,8 @@ import Home from './Components/Home/Home';
 import InventoryDetails from './Components/Inventory/InventoryDetails';
 import InventoryPage from './Components/Inventory/InventoryPage';
 import Login from './Components/Login/Login';
+import ManageInventory from './Components/Manage Inventory/ManageInventory';
+import MyItems from './Components/MyItems/MyItems';
 import NotFound from './Components/NotFound/NotFound';
 import RequireAuth from './Components/RequireAuth/RequireAuth';
 import SignUp from './Components/SignUp/SignUp';
@@ -26,6 +29,21 @@ function App() {
           <Route path='/inventory/inventoryDetails/:inventoryId' element={
             <RequireAuth>
               <InventoryDetails/>
+            </RequireAuth>
+          } />
+          <Route path='/addinventories' element={
+            <RequireAuth>
+              <AddInventory/>
+            </RequireAuth>
+          } />
+          <Route path='/manageinventories' element={
+            <RequireAuth>
+              <ManageInventory/>
+            </RequireAuth>
+          } />
+          <Route path='/myitems' element={
+            <RequireAuth>
+              <MyItems/>
             </RequireAuth>
           } />
           <Route path='*' element={<NotFound/>}/>
