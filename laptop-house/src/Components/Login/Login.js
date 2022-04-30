5bddd6b8cdc4b0861645876a2bb9348046652ec8
rafidmahin20 <submanmahin@gmail.com>
2022-04-30 22:56:45 +0600

    edited

--- laptop-house/src/Components/Login/Login.js
+++ laptop-house/src/Components/Login/Login.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import google from '../../Images/social/google.png';
+import auth from '../Firebase/Firebase.init';
 
 const Login = () => {
     const navigate = useNavigate();
+    const emailRef = useRef('');
+    const passRef = useRef('');
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+    if(user){
+        navigate(from, {replace: true});
+    }
     const navigateToSignup = () =>{
         navigate('/signup');
+    }
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passRef.current.value;
+        signInWithEmailAndPassword(email, password);
+        navigate('/');
     }
     return (
         <div className='flex items-center justify-center mt-20'>
             <div className='shadow-lg m-10 p-10 sm:w-96 md:w-96 lg:96 2xl:96 rounded border border-zinc-400'>
                 <h2 className='text-3xl text-center font-bold mb-6'>Login</h2>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className='mx-auto w-80  mb-5'>
-                    <input type="email"  className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="email" required/>
+                    <input 
+                    ref={emailRef}
+                    type="email"  className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="email" required/>
                     </div>
                     <div className='mx-auto w-80  mb-5'>
-                    <input type="password" id="floating_filled" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="password" required/>
+                    <input 
+                    ref={passRef}
+                    type="password" id="floating_filled" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900  border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="password" required/>
                     </div>
                     <div className='text-sm text-center mb-10 mt-4'><p>forgot your password? <span className='underline text-sky-500 cursor-pointer'>Reset Password</span></p></div>
                     <div className='flex justify-center'>
