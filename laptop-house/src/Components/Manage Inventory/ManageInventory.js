260566deba3bef27afe6ef074309fc6d087ea8ba
rafidmahin20 <submanmahin@gmail.com>
2022-05-02 22:56:02 +0600

    edited

--- laptop-house/src/Components/Manage Inventory/ManageInventory.js
+++ laptop-house/src/Components/Manage Inventory/ManageInventory.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const ManageInventory = () => {
     return (
-        <div>
-            <h1>hello manage</h1>
+        <div className="flex justify-center mt-10">
+          <button 
+            type="button"
+            className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Add New Items
+          </button>
         </div>
     );
 };
