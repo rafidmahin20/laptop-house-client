52c570b9e6647afc958ebccf7599e1718b6b5aad
rafidmahin20 <submanmahin@gmail.com>
2022-05-04 15:20:55 +0600

    edited

--- laptop-house/src/Components/Manage Inventory/ManageInventory.js
+++ laptop-house/src/Components/Manage Inventory/ManageInventory.js
@@ -1,78 +1,92 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import useItem from "../Hooks/useItem";
-import bin from '../../Images/delete.png'
+import bin from "../../Images/delete.png";
 
 const ManageInventory = () => {
-    const [items, setItems] = useItem();
-    const navigate = useNavigate();
-    const navigateToAddInventories = () =>{
-        navigate('/addinventories');
-    }
+  const [items, setItems] = useItem();
+  const navigate = useNavigate();
+  const navigateToAddInventories = () => {
+    navigate("/addinventories");
+  };
   return (
     <main>
       <div className="flex justify-center mt-10">
-        <button onClick={navigateToAddInventories}
+        <button
+          onClick={navigateToAddInventories}
           type="button"
           className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         >
           Add New Items
         </button>
       </div>
-      <div className='flex mt-10 mx-auto ml-10'>
-            <div className="flex mx-auto shadow-md sm:rounded-lg " style={{ height: '100%' }}>
-                <table className="w-full text-sm text-left text-gray-500">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Name
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Price
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Quantity
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                               Details
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                               Supplier Name
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            items.map(item => <>
-                                <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                        {item.name}
-                                    </th>
-                                    <td className="px-6 py-4">
-                                        {item.price}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {item.quantity}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {item.short_description}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {item.supplier_name}
-                                    </td>
-                                    <td className="px-6 py-4 text-right ">
-                                        <button className=' bg-red-500 w-8 h-8 rounded'><img src={bin} alt="" className="w-6 h-6 flex mx-auto" /></button>
-                                    </td>
-                                </tr>
-                            </>)
-                        }
-                    </tbody>
-                </table>
-            </div>
+      <div className="flex mt-10 mx-auto ml-10">
+        <div
+          className="flex mx-auto shadow-md sm:rounded-lg "
+          style={{ height: "100%" }}
+        >
+          <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Image
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Quantity
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Details
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Supplier Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <>
+                  <tr
+                    key={item._id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                      <td className="px-6 py-4 whitespace-nowrap">
+                          <img className="h-12 w-12 rounded-full" src={item.picture} alt=""/>
+                      </td>
+                    <td
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                    >
+                      {item.name}
+                    </td>
+                    <td className="px-6 py-4">{item.price}</td>
+                    <td className="px-6 py-4">{item.quantity}</td>
+                    <td className="px-6 py-4">{item.short_description}</td>
+                    <td className="px-6 py-4">{item.supplier_name}</td>
+                    <td className="px-6 py-4 text-right ">
+                      <button className=" bg-red-500 w-8 h-8 rounded">
+                        <img
+                          src={bin}
+                          alt=""
+                          className="w-6 h-6 flex mx-auto"
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </main>
   );
 };
