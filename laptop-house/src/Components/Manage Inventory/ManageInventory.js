812fb76dca385aad94c4155d0c42d4205df443e8
rafidmahin20 <submanmahin@gmail.com>
2022-05-04 13:43:40 +0600

    edited

--- laptop-house/src/Components/Manage Inventory/ManageInventory.js
+++ laptop-house/src/Components/Manage Inventory/ManageInventory.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import useItem from "../Hooks/useItem";
-import bin from '../../Images/trash-2 9.png'
+import bin from '../../Images/delete.png'
 
 const ManageInventory = () => {
     const [items, setItems] = useItem();
