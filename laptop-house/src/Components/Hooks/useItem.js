243e6ce20fe0cb01b761f1e55b9936e7977a55ce
rafidmahin20 <submanmahin@gmail.com>
2022-05-04 13:30:38 +0600

    edited

---
+++ laptop-house/src/Components/Hooks/useItem.js
@@ -0,0 +1,13 @@
+import  { useEffect, useState } from 'react';
+
+const useItem = () => {
+    const [items, setItems] = useState();
+    useEffect(() => {
+        fetch('http://localhost:5000/inventorypage')
+        .then(res => res.json())
+        .then(data => setItems(data));
+    }, [])
+    return [items];
+};
+
+export default useItem;
